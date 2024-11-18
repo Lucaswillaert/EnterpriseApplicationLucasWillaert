e8ad3cbd0283a89316448630de724f2c3b56c04f
Lucaswillaert <lucas.willaert@student.ehb.be>
2024-11-18 18:17:55 +0100

    Initial commit

---
+++ src/main/java/be/rungroup/eelucaswillaert/EeLucasWillaertApplication.java
@@ -0,0 +1,13 @@
+package be.rungroup.eelucaswillaert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeLucasWillaertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeLucasWillaertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/rungroup/eelucaswillaert/EeLucasWillaertApplicationTests.java
@@ -0,0 +1,13 @@
+package be.rungroup.eelucaswillaert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeLucasWillaertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
