71c8d9db6f29dc75dbb6311a85255d40726546c6
Lucaswillaert <lucas.willaert@student.ehb.be>
2024-11-26 15:38:40 +0100

    created Product repository, added annotations/data in the ProductDto

--- src/main/java/be/rungroup/eelucaswillaert/dto/ProductDto.java
+++ src/main/java/be/rungroup/eelucaswillaert/dto/ProductDto.java
@@ -1,4 +1,31 @@
 package be.rungroup.eelucaswillaert.dto;
 
+import be.rungroup.eelucaswillaert.model.Tag;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
 public class ProductDto {
+
+    private Long id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    private String description;
+    @NotBlank(message = "Total stock is mandatory")
+    private int totalStock;
+    @NotBlank(message = "Photo URL is mandatory")
+    private String photoUrl;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private List<Tag> tags = new ArrayList<>();
 }
--- src/main/java/be/rungroup/eelucaswillaert/repository/ProductRepository.java
+++ src/main/java/be/rungroup/eelucaswillaert/repository/ProductRepository.java
@@ -1,4 +1,16 @@
 package be.rungroup.eelucaswillaert.repository;
 
-public interface ProductRepository {
+import be.rungroup.eelucaswillaert.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+
+    Optional <Product> addToCart(Long id, int quantity);
+
+    Optional <Product> removeFromCart(Long id, int quantity);
+
+
 }
