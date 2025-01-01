ed89e650aa542d4253385a9ce0b169513d87fa4b
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-01-01 12:33:53 +0100

    made an exception for the date validation

---
+++ src/main/java/be/rungroup/eelucaswillaert/exceptions/DateValidationException.java
@@ -0,0 +1,7 @@
+package be.rungroup.eelucaswillaert.exceptions;
+
+public class DateValidationException extends RuntimeException {
+    public DateValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/be/rungroup/eelucaswillaert/exceptions/GlobalExceptionHandler.java
+++ src/main/java/be/rungroup/eelucaswillaert/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,19 @@
 package be.rungroup.eelucaswillaert.exceptions;
 
 
+import be.rungroup.eelucaswillaert.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @ExceptionHandler(RuntimeException.class)
     public String handleRuntimeException(RuntimeException ex, Model model){
@@ -18,6 +25,14 @@ public class GlobalExceptionHandler {
     public String handleFileSizeLimitExceededException(FileSizeLimitExceededException ex, Model model) {
         model.addAttribute("error", ex.getMessage());
         return "error";
+    }
+
+    @ExceptionHandler(DateValidationException.class)
+    public String handleDateValidationException(DateValidationException ex, Model model, @PathVariable Long product_id) {
+        model.addAttribute("error", ex.getMessage());
+        model.addAttribute("product", productRepository.findById(product_id)
+                .orElseThrow(() -> new IllegalArgumentException("Product niet gevonden")));
+        return "products/product-detail";
     }
 
 }
